2dab7aa0a6d3fc35a9c6b4b80662d6bdc8ed0aea
fir0j <firoj.is.available@gmail.com>
2020-05-03 15:03:20 +0530

    Updates

---
+++ precache-manifest.22e0f713b9170acf59f752837e7c80d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed043ad33ac6e39623753675e42cfda7",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "e479c70bc0cea8afb3b2",
+    "url": "/shopping-cart/static/css/main.9c2da657.chunk.css"
+  },
+  {
+    "revision": "0e4fba3fea60a54ea540",
+    "url": "/shopping-cart/static/js/2.e1b16fb4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/shopping-cart/static/js/2.e1b16fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e479c70bc0cea8afb3b2",
+    "url": "/shopping-cart/static/js/main.02789a8f.chunk.js"
+  },
+  {
+    "revision": "f78ee7b71997a9b5e1df",
+    "url": "/shopping-cart/static/js/runtime-main.2044215a.js"
+  },
+  {
+    "revision": "79901f9a134593a5dad1c6779d5d9395",
+    "url": "/shopping-cart/static/media/burger.79901f9a.jpg"
+  },
+  {
+    "revision": "f0046d71cdc17afdd3a41562552317cc",
+    "url": "/shopping-cart/static/media/pasta.f0046d71.jpg"
+  },
+  {
+    "revision": "1a4e634088a4fd70c0b1293700cdc692",
+    "url": "/shopping-cart/static/media/pizza.1a4e6340.jpg"
+  },
+  {
+    "revision": "10e443d4fbe9b28e07c8f2beee755be1",
+    "url": "/shopping-cart/static/media/sandwitch.10e443d4.jpg"
+  }
+]);
